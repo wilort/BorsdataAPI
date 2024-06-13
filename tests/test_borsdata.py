e1d5b5446785936ea6020247ad425babd51fe558
William Orton <william.orton.sorensen@gmail.com>
2024-06-13 16:42:59 +0200

    Add initial test for own function

--- tests/test_borsdata.py
+++ tests/test_borsdata.py
@@ -5,5 +5,6 @@ def test_borsdata():
     shit = borsdata_client.instruments_with_meta_data()
 
 def test_get_stocks_with_kpi():
-    pass
+    borsdata_client = BorsdataClient()
+    shit = borsdata_client.get_stocks_with_kpi()
     
